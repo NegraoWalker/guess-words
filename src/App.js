76d671efcf76862112ccd950aabae17d77fd3a3f
walkerNegrão <walker.esteves.negrao@gmail.com>
2022-07-13 20:11:12 -0300

    Criação e importação de novos components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
+// CSS da aplicação:
 import './App.css';
+// Hooks da aplicação:
+import { useCallback, useEffect, useState } from 'react';
+// Dados da aplicação:
+import { wordsList } from './data/words'
+// Components da aplicação:
 import StartScreen from './components/StartScreen';
+import Game from './components/Game';
+import GameOver from './components/GameOver';
 
+//Estágios do nosso Jogo:
+const stages = [
+  { id: 1, name: 'start' },
+  { id: 2, name: 'game' },
+  { id: 3, name: 'end' },
+]
 function App() {
+
+  const [gameStage, setGameStage] = useState(stages[0].name)
+  const [words] = useState(wordsList)
+  console.log(wordsList)
+
   return (
     <div className="App">
-      <StartScreen />
+      {gameStage === 'start' && <StartScreen />} {/*Se o estágio do game for o start eu exibo meu component StartScreen*/}
+      {gameStage === 'game' && <Game />}
+      {gameStage === 'end' && <GameOver />}
     </div>
   );
 }
---
+++ src/components/GameOver.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './GameOver.css'
+
+const GameOVer = () => {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default GameOVer
